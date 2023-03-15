247444662df85c65c198c276184ccf02d4a85b8c
kevin.coatsworth <29205960+kevincoatsworth@users.noreply.github.com>
2023-03-15 10:50:43 +0000

    CTCP-1952: Added index

--- test/controllers/document/GoodsItemNumberControllerSpec.scala
+++ test/controllers/document/GoodsItemNumberControllerSpec.scala
@@ -1,10 +1,27 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.document
 
 import base.{AppWithDefaultMockFixtures, SpecBase}
 import forms.GoodsItemNumberFormProvider
 import models.NormalMode
+import navigation.DocumentsNavigatorProvider
 import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.{reset, when}
+import org.mockito.Mockito.when
 import pages.document.GoodsItemNumberPage
 import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
@@ -16,10 +33,10 @@ import scala.concurrent.Future
 
 class GoodsItemNumberControllerSpec extends SpecBase with AppWithDefaultMockFixtures {
 
-  private val formProvider = new GoodsItemNumberFormProvider()
-  private val form         = formProvider("document.goodsItemNumber")
-  private val mode         = NormalMode
-  private lazy val goodsItemNumberRoute = routes.GoodsItemNumberController.onPageLoad(lrn, mode).url
+  private val formProvider              = new GoodsItemNumberFormProvider()
+  private val form                      = formProvider("document.goodsItemNumber")
+  private val mode                      = NormalMode
+  private lazy val goodsItemNumberRoute = routes.GoodsItemNumberController.onPageLoad(lrn, mode, documentIndex).url
 
   override def guiceApplicationBuilder(): GuiceApplicationBuilder =
     super
@@ -41,26 +58,26 @@ class GoodsItemNumberControllerSpec extends SpecBase with AppWithDefaultMockFixt
       status(result) mustEqual OK
 
       contentAsString(result) mustEqual
-        view(form, lrn, mode)(request, messages).toString
+        view(form, lrn, mode, documentIndex)(request, messages).toString
     }
 
     "must populate the view correctly on a GET when the question has previously been answered" in {
 
-      val userAnswers = emptyUserAnswers.setValue(GoodsItemNumberPage, "test string")
+      val userAnswers = emptyUserAnswers.setValue(GoodsItemNumberPage(documentIndex), "test1")
       setExistingUserAnswers(userAnswers)
 
       val request = FakeRequest(GET, goodsItemNumberRoute)
 
       val result = route(app, request).value
 
-      val filledForm = form.bind(Map("value" -> "test string"))
+      val filledForm = form.bind(Map("value" -> "test1"))
 
       val view = injector.instanceOf[GoodsItemNumberView]
 
       status(result) mustEqual OK
 
       contentAsString(result) mustEqual
-        view(filledForm, lrn, mode)(request, messages).toString
+        view(filledForm, lrn, mode, documentIndex)(request, messages).toString
     }
 
     "must redirect to the next page when valid data is submitted" in {
@@ -70,7 +87,7 @@ class GoodsItemNumberControllerSpec extends SpecBase with AppWithDefaultMockFixt
       when(mockSessionRepository.set(any())(any())) thenReturn Future.successful(true)
 
       val request = FakeRequest(POST, goodsItemNumberRoute)
-        .withFormUrlEncodedBody(("value", "test string"))
+        .withFormUrlEncodedBody(("value", "test1"))
 
       val result = route(app, request).value
 
@@ -85,7 +102,7 @@ class GoodsItemNumberControllerSpec extends SpecBase with AppWithDefaultMockFixt
 
       val invalidAnswer = ""
 
-      val request = FakeRequest(POST, goodsItemNumberRoute).withFormUrlEncodedBody(("value", ""))
+      val request    = FakeRequest(POST, goodsItemNumberRoute).withFormUrlEncodedBody(("value", ""))
       val filledForm = form.bind(Map("value" -> invalidAnswer))
 
       val result = route(app, request).value
@@ -95,7 +112,7 @@ class GoodsItemNumberControllerSpec extends SpecBase with AppWithDefaultMockFixt
       val view = injector.instanceOf[GoodsItemNumberView]
 
       contentAsString(result) mustEqual
-        view(filledForm, lrn, mode)(request, messages).toString
+        view(filledForm, lrn, mode, documentIndex)(request, messages).toString
     }
 
     "must redirect to Session Expired for a GET if no existing data is found" in {
@@ -116,7 +133,7 @@ class GoodsItemNumberControllerSpec extends SpecBase with AppWithDefaultMockFixt
       setNoExistingUserAnswers()
 
       val request = FakeRequest(POST, goodsItemNumberRoute)
-        .withFormUrlEncodedBody(("value", "test string"))
+        .withFormUrlEncodedBody(("value", "test1"))
 
       val result = route(app, request).value
 
--- test/views/document/GoodsItemNumberViewSpec.scala
+++ test/views/document/GoodsItemNumberViewSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package views.document
 
 import forms.GoodsItemNumberFormProvider
@@ -16,7 +32,7 @@ class GoodsItemNumberViewSpec extends InputTextViewBehaviours[String] {
   override def form: Form[String] = new GoodsItemNumberFormProvider()(prefix)
 
   override def applyView(form: Form[String]): HtmlFormat.Appendable =
-    injector.instanceOf[GoodsItemNumberView].apply(form, lrn, NormalMode)(fakeRequest, messages)
+    injector.instanceOf[GoodsItemNumberView].apply(form, lrn, NormalMode, documentIndex)(fakeRequest, messages)
 
   implicit override val arbitraryT: Arbitrary[String] = Arbitrary(Gen.alphaStr)
 
