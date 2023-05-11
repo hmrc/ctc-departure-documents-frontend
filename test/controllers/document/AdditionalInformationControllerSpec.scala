d92f2c069ed70e4ecd86388b015476f16da8effe
BledarFeratiEY <104501743+BledarFeratiEY@users.noreply.github.com>
2023-05-11 14:13:04 +0100

    CTCP-2849: Add spec for additionalinformation page

--- test/controllers/document/AdditionalInformationControllerSpec.scala
+++ test/controllers/document/AdditionalInformationControllerSpec.scala
@@ -1,21 +1,55 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.document
 
 import base.{AppWithDefaultMockFixtures, SpecBase}
+import forms.AdditionalInformationFormProvider
+import models.NormalMode
+import navigation.DocumentNavigatorProvider
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import pages.document.AdditionalInformationPage
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import views.html.document.AdditionalInformationView
 
+import scala.concurrent.Future
+
 class AdditionalInformationControllerSpec extends SpecBase with AppWithDefaultMockFixtures {
 
-  private lazy val additionalInformationRoute = routes.AdditionalInformationController.onPageLoad(lrn).url
+  private val formProvider                            = new AdditionalInformationFormProvider()
+  private val form                                    = formProvider("document.additionalInformation", documentIndex.display)
+  private val mode                                    = NormalMode
+  private lazy val documentAdditionalInformationRoute = routes.AdditionalInformationController.onPageLoad(lrn, mode, documentIndex).url
 
-  "AdditionalInformation Controller" - {
+  override def guiceApplicationBuilder(): GuiceApplicationBuilder =
+    super
+      .guiceApplicationBuilder()
+      .overrides(bind(classOf[DocumentNavigatorProvider]).toInstance(fakeDocumentNavigatorProvider))
+
+  "ItemDescription Controller" - {
 
     "must return OK and the correct view for a GET" in {
 
       setExistingUserAnswers(emptyUserAnswers)
 
-      val request = FakeRequest(GET, additionalInformationRoute)
+      val request = FakeRequest(GET, documentAdditionalInformationRoute)
       val result  = route(app, request).value
 
       val view = injector.instanceOf[AdditionalInformationView]
@@ -23,7 +57,88 @@ class AdditionalInformationControllerSpec extends SpecBase with AppWithDefaultMo
       status(result) mustEqual OK
 
       contentAsString(result) mustEqual
-        view(lrn)(request, messages).toString
+        view(form, lrn, mode, documentIndex)(request, messages).toString
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = emptyUserAnswers.setValue(AdditionalInformationPage(documentIndex), "testString")
+      setExistingUserAnswers(userAnswers)
+
+      val request = FakeRequest(GET, documentAdditionalInformationRoute)
+
+      val result = route(app, request).value
+
+      val filledForm = form.bind(Map("value" -> "testString"))
+
+      val view = injector.instanceOf[AdditionalInformationView]
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(filledForm, lrn, mode, documentIndex)(request, messages).toString
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      when(mockSessionRepository.set(any())(any())) thenReturn Future.successful(true)
+
+      val request = FakeRequest(POST, documentAdditionalInformationRoute)
+        .withFormUrlEncodedBody(("value", "testString"))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual onwardRoute.url
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      val invalidAnswer = ""
+
+      val request    = FakeRequest(POST, documentAdditionalInformationRoute).withFormUrlEncodedBody(("value", ""))
+      val filledForm = form.bind(Map("value" -> invalidAnswer))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual BAD_REQUEST
+
+      val view = injector.instanceOf[AdditionalInformationView]
+
+      contentAsString(result) mustEqual
+        view(filledForm, lrn, mode, documentIndex)(request, messages).toString
+    }
+
+    "must redirect to Session Expired for a GET if no existing data is found" in {
+
+      setNoExistingUserAnswers()
+
+      val request = FakeRequest(GET, documentAdditionalInformationRoute)
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual frontendAppConfig.sessionExpiredUrl
+    }
+
+    "must redirect to Session Expired for a POST if no existing data is found" in {
+
+      setNoExistingUserAnswers()
+
+      val request = FakeRequest(POST, documentAdditionalInformationRoute)
+        .withFormUrlEncodedBody(("value", "test string"))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual frontendAppConfig.sessionExpiredUrl
     }
   }
 }
--- test/views/document/AdditionalInformationViewSpec.scala
+++ test/views/document/AdditionalInformationViewSpec.scala
@@ -1,13 +1,35 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package views.document
 
+import forms.AdditionalInformationFormProvider
+import forms.Constants.maxAdditionalInformationLength
+import models.NormalMode
+import play.api.data.Form
 import play.twirl.api.HtmlFormat
-import views.behaviours.ViewBehaviours
+import views.behaviours.CharacterCountViewBehaviours
 import views.html.document.AdditionalInformationView
 
-class AdditionalInformationViewSpec extends ViewBehaviours {
+class AdditionalInformationViewSpec extends CharacterCountViewBehaviours {
 
-  override def view: HtmlFormat.Appendable =
-    injector.instanceOf[AdditionalInformationView].apply(lrn)(fakeRequest, messages)
+  override def form: Form[String] = new AdditionalInformationFormProvider()(prefix, documentIndex.display)
+
+  override def applyView(form: Form[String]): HtmlFormat.Appendable =
+    injector.instanceOf[AdditionalInformationView].apply(form, lrn, NormalMode, documentIndex)(fakeRequest, messages)
 
   override val prefix: String = "document.additionalInformation"
 
@@ -15,5 +37,11 @@ class AdditionalInformationViewSpec extends ViewBehaviours {
 
   behave like pageWithBackLink()
 
+  behave like pageWithSectionCaption("Documents")
+
   behave like pageWithHeading()
+
+  behave like pageWithCharacterCount(maxAdditionalInformationLength)
+
+  behave like pageWithSubmitButton("Save and continue")
 }
