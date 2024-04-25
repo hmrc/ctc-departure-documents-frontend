122d404896e2ba78f2ec541e441b0226557a6b4b
malaw-hmrc <malawarayan.ganesan@digital.hmrc.gov.uk>
2024-04-25 19:24:53 +0100

    feat(update-signout-url): update signout url as per ctcp-5216

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -51,7 +51,8 @@ class FrontendAppConfig @Inject() (configuration: Configuration, servicesConfig:
 
   def keepAliveUrl(lrn: LocalReferenceNumber): String = s"$departureHubUrl/$lrn/keep-alive"
 
-  def signOutUrl(lrn: LocalReferenceNumber): String = s"$departureHubUrl/$lrn/delete-lock"
+  def signOutUrl(lrn: LocalReferenceNumber): String        = s"$departureHubUrl/$lrn/delete-lock"
+  def sessionExpiredUrl(lrn: LocalReferenceNumber): String = s"$departureHubUrl/this-service-has-been-reset/$lrn"
 
   lazy val maxPreviousDocuments: Int   = configuration.get[Int]("limits.maxPreviousDocuments")
   lazy val maxSupportingDocuments: Int = configuration.get[Int]("limits.maxSupportingDocuments")
