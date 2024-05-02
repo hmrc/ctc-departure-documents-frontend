a0ec488fb279043b68da6f02fa9dbbff0aaf6d89
malaw-hmrc <malawarayan.ganesan@digital.hmrc.gov.uk>
2024-05-02 20:37:58 +0100

    feat(CTP-5216): update signOut url and refactor code

--- test/controllers/document/LineItemNumberControllerSpec.scala
+++ test/controllers/document/LineItemNumberControllerSpec.scala
@@ -126,7 +126,7 @@ class LineItemNumberControllerSpec extends SpecBase with AppWithDefaultMockFixtu
 
       status(result) mustEqual SEE_OTHER
 
-      redirectLocation(result).value mustEqual frontendAppConfig.sessionExpiredUrl
+      redirectLocation(result).value mustEqual frontendAppConfig.sessionExpiredUrl(lrn)
     }
 
     "must redirect to Session Expired for a POST if no existing data is found" in {
@@ -140,7 +140,7 @@ class LineItemNumberControllerSpec extends SpecBase with AppWithDefaultMockFixtu
 
       status(result) mustEqual SEE_OTHER
 
-      redirectLocation(result).value mustEqual frontendAppConfig.sessionExpiredUrl
+      redirectLocation(result).value mustEqual frontendAppConfig.sessionExpiredUrl(lrn)
     }
   }
 }
--- test/controllers/document/TypeControllerSpec.scala
+++ test/controllers/document/TypeControllerSpec.scala
@@ -144,7 +144,7 @@ class TypeControllerSpec extends SpecBase with AppWithDefaultMockFixtures with G
       val result = route(app, request).value
 
       status(result) mustEqual SEE_OTHER
-      redirectLocation(result).value mustEqual frontendAppConfig.sessionExpiredUrl
+      redirectLocation(result).value mustEqual frontendAppConfig.sessionExpiredUrl(lrn)
     }
 
     "must redirect to Session Expired for a POST if no existing data is found" in {
@@ -158,7 +158,7 @@ class TypeControllerSpec extends SpecBase with AppWithDefaultMockFixtures with G
 
       status(result) mustEqual SEE_OTHER
 
-      redirectLocation(result).value mustEqual frontendAppConfig.sessionExpiredUrl
+      redirectLocation(result).value mustEqual frontendAppConfig.sessionExpiredUrl(lrn)
     }
   }
 }
