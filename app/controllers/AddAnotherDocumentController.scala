c3d3195e5452f3dd724227bdfbdcbf0f76b5de07
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2024-07-03 08:06:37 +0100

    CTCP-5506: update dependency and optimise import

--- app/controllers/AddAnotherDocumentController.scala
+++ app/controllers/AddAnotherDocumentController.scala
@@ -23,7 +23,6 @@ import models.{LocalReferenceNumber, Mode, NormalMode}
 import play.api.data.Form
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import viewModels.AddAnotherDocumentViewModel
 import viewModels.AddAnotherDocumentViewModel.AddAnotherDocumentViewModelProvider
