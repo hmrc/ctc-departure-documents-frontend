4ff547c37d429b554caa238d672d212484bec3c3
Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
2025-02-13 11:36:55 +0000

    Revert "CTCP-6206 Update play-frontend-hmrc to 11.11.0"
    This reverts commit 3d4efcd87875c899c84ffafe9acc3e8a61e248c5.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
 // prevent resubmit warning
 if (window.history && window.history.replaceState && typeof window.history.replaceState === 'function') {
-    window.history.replaceState(null, null, window.location.href);
+  window.history.replaceState(null, null, window.location.href);
 }
